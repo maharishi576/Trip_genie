fb21614c32178f4d920d7c4cb078e0a5a3853b6c
Rishi <maharishi576@gmail.com>
2025-09-16 00:13:01 +0530

    SETUP JAVA

---
+++ trip-genie-backend/src/main/java/com/example/trip_genie_backend/TripGenieBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.trip_genie_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripGenieBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripGenieBackendApplication.class, args);
+	}
+
+}
